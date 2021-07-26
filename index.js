88686ce3571b5890acd7c5213a95a36d8b1b91f6
Gernot H. Reichel <gernot.reichel@me.com>
2021-07-26 10:22:13 +0200

    new bucket: fritzbox; also added comments

--- index.js
+++ index.js
@@ -8,9 +8,18 @@ require('dotenv').config();
 // cycle time
 const DELAY = process.env.DELAY || 5;
 
+console.log(process.env);
+
+/**
+ * connect to fritz box; config see .env
+ */
 connectFritzBox = async () => {
     return await Fritzbox.fritz();  // options are taken from process.env that are loaded from .env file
 }
+
+/**
+ * connect to influx database; config see .env
+ */
 connectInfluxDB = async (data) => {
 
     // You can generate a Token from the "Tokens Tab" in the UI
@@ -20,25 +29,41 @@ connectInfluxDB = async (data) => {
     return new InfluxDB({ url, token });
 }
 
+/**
+ * write data to influx database
+ */
 write2InfluxDB = async (client, data) => {
-    const org = process.env.INFLUXBD_CLOUD_ORGANIZATION
-    const bucket = process.env.INFLUXBD_CLOUD_BUCKET
+    const org = process.env.INFLUXBD_CLOUD_ORGANIZATION;
+    const bucket = process.env.INFLUXBD_CLOUD_BUCKET;
 
-    const writeApi = client.getWriteApi(org, bucket)
+    const writeApi = client.getWriteApi(org, bucket);
     writeApi.useDefaultTags({ bandwith: 'bandwidth' });
 
-    writeApi.writePoint(new Point('bndw').intField('upstream_current', data.upCurrent))
-    writeApi.writePoint(new Point('bndw').intField('downstream_current', data.downCurrent))
-    writeApi.writePoint(new Point('bndw').floatField('upstream_mbit', data.up))
-    writeApi.writePoint(new Point('bndw').floatField('downstream_mbit', data.down))
+    // bandwidth
+    writeApi.writePoint(new Point('bndw').intField('upstream_current', data.upCurrent));
+    writeApi.writePoint(new Point('bndw').intField('downstream_current', data.downCurrent));
+    writeApi.writePoint(new Point('bndw').floatField('upstream_mbit', data.up));
+    writeApi.writePoint(new Point('bndw').floatField('downstream_mbit', data.down));
+
+    // max values
+    writeApi.writePoint(new Point('max').floatField('downstream_max', data.downMax));
+    writeApi.writePoint(new Point('max').floatField('upstream_max', data.upMax));
+
 
     writeApi.close()
         .catch(e => {
-            console.error(e)
-            console.log('Finished ERROR')
+            console.error(e);
+            console.log('Finished ERROR');
         });
 }
-
+/**
+ * calculate bandwidth from fritz box data
+ * 
+ * upCurrent:   current upstream in MBit
+ * downCurrent: current downstream in MBit
+ * factUp:      procentage value of upstream over max upstream
+ * factDown:    procentage value of downstream over max downstream
+ */
 bandwith = async (con) => {
     const usage = await con.getBandwithUsage();
     usage.up = usage.upCurrent / 1024 / 1024;
@@ -57,7 +82,7 @@ run = async () => {
             const usage = await bandwith(conFritz);
             await write2InfluxDB(conInflux, usage);
             console.log(`\n${new Date().toISOString()}`);
-            console.log(usage)
+            console.log(usage);
         } catch (error) {
             console.error(error);
             conFritz = await connectFritzBox();
